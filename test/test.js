1c13f690276c0a88bd10656e225642f328d756e4
Hoang Gia Minh <hgminh95@gmail.com>
2016-02-16 15:23:24 +0000

    Add mocha and travis for testing

---
+++ test/test.js
@@ -0,0 +1,17 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../app');
+var should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('Static pages', function() {
+  it('should return main page', function(done) {
+    chai.request(server)
+      .get('/')
+      .end(function(err, res){
+        res.should.have.status(200);
+        done();
+      });
+  });
+})
