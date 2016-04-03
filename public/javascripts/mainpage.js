a5141509a7783cf131e590ae1bc0ea73a33ddf4f
Hoang Gia Minh <hgminh95@gmail.com>
2016-04-03 11:05:00 +0700

    Add warning message in home page

--- public/javascripts/mainpage.js
+++ public/javascripts/mainpage.js
@@ -5,3 +5,9 @@ $(window).scroll(function() {
     $('#nav').removeClass('shrink');
   }
 });
+
+$(document).ready(function() {
+  $(".alert").fadeTo(10000, 500).slideUp(500, function(){
+    $(".alert").alert('close');
+  });
+})
