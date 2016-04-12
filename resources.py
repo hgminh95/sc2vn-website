4510c844986947fd0f407a7a2c5b95c7dad7cda4
Hoang Gia Minh <hgminh95@gmail.com>
2016-04-12 20:51:49 +0700

    Refactor code in get-resources script

--- resources.py
+++ resources.py
@@ -2,58 +2,72 @@ import urllib
 import sys
 import os
 
-fontawesome_url = 'https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css'
-eot_url = '/../../fonts/fontawesome-webfont.eot?v=4.5.0'
-woff2_url = '/../../fonts/fontawesome-webfont.woff2?v=4.5.0'
-woff_url = '/../../fonts/fontawesome-webfont.woff?v=4.5.0'
-ttf_url = '/../../fonts/fontawesome-webfont.ttf?v=4.5.0'
-svg_url = '/../../fonts/fontawesome-webfont.svg?v=4.5.0#fontawesomeregular'
+resources = [
+    {
+        "name": "Font awesome - css",
+        "url": "https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css",
+        "dir": "public/stylesheets/fontawesome.min.css"
+    },
+    {
+        "name": "Font awesome - eot",
+        "url": "https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/fonts/fontawesome-webfont.eot?v=4.5.0",
+        "dir": "public/fonts/fontawesome-webfont.eot"
+    },
+    {
+        "name": "Font awesome - woff2",
+        "url": "https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/fonts/fontawesome-webfont.woff2?v=4.5.0",
+        "dir": "public/fonts/fontawesome-webfont.woff2"
+    },
+    {
+        "name": "Font awesome - woff",
+        "url": "https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/fonts/fontawesome-webfont.woff?v=4.5.0",
+        "dir": "public/fonts/fontawesome-webfont.woff"
+    },
+    {
+        "name": "Font awesome - ttf",
+        "url": "https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/fonts/fontawesome-webfont.ttf?v=4.5.0",
+        "dir": "public/fonts/fontawesome-webfont.ttf"
+    },
+    {
+        "name": "Font awesome - svg",
+        "url": "https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/fonts/fontawesome-webfont.svg?v=4.5.0#fontawesomeregular",
+        "dir": "public/fonts/fontawesome-webfont.svg"
+    },
+    {
+        "name": "JQuery",
+        "url": "https://code.jquery.com/jquery-2.2.2.min.js",
+        "dir": "public/javascripts/jquery.min.js"
+    },
+    {
+        "name": "BootstrapJS",
+        "url": "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js",
+        "dir": "public/javascripts/bootstrap.min.js"
+    },
+    {
+        "name": "ChartJS",
+        "url": "https://cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.2/Chart.min.js",
+        "dir": "public/javascripts/chartjs.min.js"
+    },
+    {
+        "name": "Pace",
+        "url": "https://raw.githubusercontent.com/HubSpot/pace/v1.0.0/pace.min.js",
+        "dir": "public/javascripts/pace.min.js"
+    }
+]
 
-jquery_url = 'https://code.jquery.com/jquery-2.2.2.min.js'
+def getResource(res):
+    sys.stdout.write("{0} - Downloading...".format(res["name"]))
+    sys.stdout.flush()
 
-bootstrapjs_url = 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js'
+    fetcher = urllib.URLopener()
+    fetcher.retrieve(res["url"], res["dir"])
+    
+    sys.stdout.write("\r{0} - Done          \n".format(res["name"]))
+    sys.stdout.flush()    
 
-chartjs_url = 'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.2/Chart.min.js'
+if not os.path.exists("public/fonts"):
+    os.makedirs("public/fonts")
 
-if not os.path.exists('public/fonts'):
-    os.makedirs('public/fonts')
+for res in resources:
+    getResource(res)
 
-# Font awesome
-sys.stdout.write("Font Awesome - Downloading...")
-sys.stdout.flush()
-fontawesome = urllib.URLopener()
-fontawesome.retrieve(fontawesome_url, "public/stylesheets/fontawesome.min.css")
-
-webfont = urllib.URLopener()
-webfont.retrieve(fontawesome_url + eot_url, 'public/fonts/fontawesome-webfont.eot')
-webfont.retrieve(fontawesome_url + woff2_url, 'public/fonts/fontawesome-webfont.woff2')
-webfont.retrieve(fontawesome_url + woff_url, 'public/fonts/fontawesome-webfont.woff')
-webfont.retrieve(fontawesome_url + ttf_url, 'public/fonts/fontawesome-webfont.ttf')
-webfont.retrieve(fontawesome_url + svg_url, 'public/fonts/fontawesome-webfont.svg')
-
-sys.stdout.write("\rFont Awesome - Done          \n")
-sys.stdout.flush()
-
-# Jquery
-sys.stdout.write("Jquery - Downloading...")
-sys.stdout.flush()
-fontawesome = urllib.URLopener()
-fontawesome.retrieve(jquery_url, "public/javascripts/jquery.min.js")
-sys.stdout.write("\rJquery - Done          \n")
-sys.stdout.flush()
-
-# Bootstrap
-sys.stdout.write("BootstrapJS - Downloading...")
-sys.stdout.flush()
-fontawesome = urllib.URLopener()
-fontawesome.retrieve(bootstrapjs_url, "public/javascripts/bootstrap.min.js")
-sys.stdout.write("\rBootstrapJS - Done          \n")
-sys.stdout.flush()
-
-# Chart JS
-sys.stdout.write("ChartJS - Downloading...")
-sys.stdout.flush()
-chartjs = urllib.URLopener()
-chartjs.retrieve(chartjs_url, "public/javascripts/chartjs.min.js")
-sys.stdout.write("\rChartJS - Done          \n")
-sys.stdout.flush()
