f2d995cf540001367f33d706738a6344e4bc488d
Hoang Gia Minh <hgminh95@gmail.com>
2016-04-19 22:41:38 +0700

    Add 404 page

--- app/middlewares/permission.js
+++ app/middlewares/permission.js
@@ -3,21 +3,21 @@
 // must use after users.loadCurrentUser
 exports.isUser = function(req, res, next) {
   if (!req.isAuthenticated())
-    return res.render(404)
+    return res.render('404')
 
   next()
 }
 
 exports.isModerator = function(req, res, next) {
   if (!req.isAuthenticated() || (req.user.role != 'mod' && req.user.role != 'admin'))
-    return res.render(404)
+    return res.render('404')
 
   next()
 }
 
 exports.isAdmin = function(req, res, next) {
   if (!req.isAuthenticated() || req.user.role == 'admin')
-    return res.render(404)
+    return res.render('404')
 
   next()
 }
@@ -25,7 +25,7 @@ exports.isAdmin = function(req, res, next) {
 // must use after articles.load
 exports.isArticleAuthor = function(req, res, next) {
   if (!req.isAuthenticated() || !req.article || !req.article.author.equals(req.user._id))
-    return res.render(404)
+    return res.render('404')
 
   next()
 }
